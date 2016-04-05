23396a4b49f42b7ffd6e964d0c9e6cb4dca29fa0
Geeku <ggongpei@qq.com>
2016-04-05 15:04:50 +0800

    add some funcitons

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,8 @@
 
         constructor : i,
         selector    : null,
+        selectorVal : '',
+        selectorPointer : 0,
 
         LOG_MESSAGE : {
             log   : '[MESSAGE] ',
@@ -45,7 +47,10 @@
                 return this;
             }
 
-            this.selector = document.querySelectorAll(value.trim());
+            this.selectorVal = value;
+            this.selector    = document.querySelectorAll(value.trim());
+            //NodeList to Array
+            this.selector    = Array.prototype.slice.call(this.selector);
 
             return this;
 
@@ -55,7 +60,12 @@
 
             if(arguments.length > 1) {
 
-                this.selector.setAttribute(name, value);
+                if(!Array.isArray(this.selector)) {
+                    this.selector.setAttribute(name, value);
+                } else {
+                    this.selector[this.selectorPointer].setAttribute(name, value);
+                }
+
 
                 if(arguments.length > 2) {
                     this.warn('Too many arguments.');
@@ -68,20 +78,44 @@
 
         on : function(eventType, handler, useCapture) {
 
+            var self = this;
             if(arguments.length < 2) {
-                this.error('function requires at least two arguments.');
+                this.error('function requires at least two arguments.function on(eventType, handler, useCapture)');
                 return;
             }
 
             useCapture = useCapture || false;
-            if(this.selector) {
-                if(Array.isArray(this.selector)) {
-                    this.selector.forEach(function(element) {
-                        this._addEvent(element, eventType.replace(/^on/, ''), handler, useCapture);
-                    })
-                }
+            eventType  = eventType.replace(/^on/i, '');
+            if(this.selector.length > 0) {
+
+                this.selector.forEach(function(element) {
+                    self._addEvent(element, eventType, handler, useCapture);
+                });
+
+            } else {
+
+                this._addEvent(document, eventType, function(e) {
+
+                    var event    = window.event || e,
+                        i        = 0,
+                        nodeList = document.querySelectorAll(this.selectorVal);
+
+                    for(i in nodeList) {
+                        if(nodeList[i] === event.target) {
+                            console.log(true);
+                        }
+                    }
+                });
             }
 
+        },
+
+        _addEvent : function(element, eventType, handler, useCapture) {
+            if(document.addEventListener) {
+                element.addEventListener(eventType, handler, useCapture);
+            } else {
+                element.attachEvent('on' + eventType, handler);
+            }
         },
 
         data : function(key, value) {
@@ -96,13 +130,51 @@
             //TODO
         },
 
-        _addEvent : function(element, eventType, handler, useCapture) {
-             if(document.addEventListener) {
-                element.addEventListener(eventType, handler, useCapture);
-             } else {
-                 element.attachEvent('on' + eventType, handler);
-             }
+        append : function(value) {
+            //TODO
         },
+
+        prepend : function(value) {
+            //TODO
+        },
+
+        parent : function() {
+            //TODO
+        },
+
+        text : function() {
+            //TODO
+        },
+
+        remove : function() {
+            //TODO
+        },
+
+        html : function(value) {
+
+            if(!value) {
+
+                if(!Array.isArray(this.selector)) {
+                    return this.selector.innerHTML;
+                } else {
+                    return this.selector[this.selectorPointer].innerHTML;
+                }
+
+            } else {
+
+                if(!Array.isArray(this.selector)) {
+                    this.selector.innerHTML = value;
+                } else {
+                    this.selector[this.selectorPointer].innerHTML = value;
+                }
+            }
+        },
+
+        eq : function(orderNum) {
+            this.selectorPointer = orderNum;
+
+            return this;
+        }
 
     };
 
